ab6363b18a4593cb87260c73d0a47ccb2b47e93d
EchoDemo <1761910310@qq.com>
2017-03-18 23:42:58 +0800

    Create getchar&&putchar.cpp

---
+++ Unit 2/2.2/getchar&&putchar.cpp
@@ -0,0 +1,20 @@
+
+
+#include "stdafx.h"
+#include<cstdio>
+#include<cstdlib>
+
+int main()
+{
+	char c1, c2,c3;
+	c1 = getchar();
+	getchar();//并没有进行存储，
+  c2 = getchar();
+	c3 = getchar();
+	putchar(c1);
+	putchar(c2);
+	putchar(c3);
+	system("pause");
+  return 0;
+}
+
