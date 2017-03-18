7bb3f96c7fba792054c71882ec0ad04e45219748
EchoDemo <1761910310@qq.com>
2017-03-18 22:41:43 +0800

    Create #define_other.cpp

---
+++ Unit 2/2.1/#define_other.cpp
@@ -0,0 +1,17 @@
+//使用ADD（a,b）来代替a+b的功能
+
+#include "stdafx.h"
+#include<cstdio>
+#include<cstdlib>
+
+#define ADD(a,b) ((a)+(b)) //括号必须加，这里是错的：#define CAL(x) (x*2+1)
+
+
+int main()
+{
+	int num1 = 3, num2 = 5;
+	printf("%d\n", ADD(num1, num2));//宏定义是直接将对应的部分进行替换。printf("%d\n",CAL(a+1));
+	system("pause");
+    return 0;
+}
+
