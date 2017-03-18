fe27ca683a1b277216644177315494fe94bd75ca
EchoDemo <1761910310@qq.com>
2017-03-18 22:37:54 +0800

    Create #define.cpp

---
+++ Unit 2/2.1/#define.cpp
@@ -0,0 +1,17 @@
+//define和const的不同在于，末尾不需要加分号并且不需要声明常量的类型，当然它还可以用来定义任何语句或者片段，不过不推荐使用
+
+
+#include "stdafx.h"
+#include<cstdio>
+#include<cstdlib>
+
+#define pi 3.14   //定义常量；
+
+int main()
+{
+	double r = 3;
+	printf("%.2f\n", pi*r*r);//求圆的面积；
+  system("pause");
+	return 0;
+}
+
