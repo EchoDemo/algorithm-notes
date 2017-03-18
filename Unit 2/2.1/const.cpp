f85ea1ca188ef48118fc5a1c7dfeead184ab4295
EchoDemo <1761910310@qq.com>
2017-03-18 22:34:43 +0800

    Create const.cpp

---
+++ Unit 2/2.1/const.cpp
@@ -0,0 +1,16 @@
+
+
+#include "stdafx.h"
+#include<cstdio>
+#include<cstdlib>
+
+const double pi = 3.14;   //定义常量；
+
+int main()
+{
+	double r = 3;
+	printf("%.2f\n", pi*r*r);
+	system("pause");
+    return 0;
+}
+
