ec3b7364b88d813a22a8170ba9031849a76871a5
EchoDemo <1761910310@qq.com>
2017-03-18 22:33:08 +0800

    Create type_cast.cpp

---
+++ Unit 2/2.1/type_cast.cpp
@@ -0,0 +1,24 @@
+//c语言好像不管是向下还是向上强制类型转换都会出错，所以最好需要类型转换时都使用强制类型转换更安全
+
+
+#include "stdafx.h"
+#include<cstdio>
+#include<cstdlib>
+
+
+int main()
+{
+	double r = 12.56;
+	int a = 3, b = 5;
+	//double c = a + 3.5;
+	printf("%d\n", (int)r);
+	printf("%d\n", a / b);
+	printf("%.1f\n", (double)a / (double)b);
+
+	printf("%d\n", r);
+	printf("%f%f\n", (double)a, b);
+	//printf("%f\n", c);
+	system("pause");
+    return 0;
+}
+
