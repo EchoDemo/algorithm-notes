cc2b520daaac43d0321c6f3e6efaf3037ad08a7b
EchoDemo <1761910310@qq.com>
2017-03-18 23:06:49 +0800

    Update arithmetic_operator.cpp

--- Unit 2/2.1/arithmetic_operator.cpp
+++ Unit 2/2.1/arithmetic_operator.cpp
@@ -6,13 +6,15 @@
 
 int main()
 {
-	int a = 5, b = 4,m=5,n=6;
-	double c = 1.23, d = 4;
-	printf("%d %d\n", a + b, a - b);
-	printf("%f\n",c*d);
-  printf("%d %d %d\n",a/b,a/c,a/d);	
+  int a = 5, b = 4,m=5,n=6;
+  double c = 1.23, d = 4;
+  printf("%d %d\n", a + b, a - b);//加减
+  printf("%f\n",c*d);//乘
+  printf("%d %d %d\n",a/b,a/c,a/d);//除
+  printf("%d %d\n",a%b,a%c);//取模
+  printf("%d %d %d %d\n",a++,b--,++c,--d);//自增与自减
   system("pause");
-    return 0;
+  return 0;
 }
 
 
