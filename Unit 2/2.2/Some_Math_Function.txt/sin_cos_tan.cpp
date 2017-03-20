ebfdc40d917c6f341cb41b8555376c9e9c31ce5e
EchoDemo <1761910310@qq.com>
2017-03-20 09:03:15 +0800

    Update sin_cos_tan.cpp

--- Unit 2/2.2/Some_Math_Function.txt/sin_cos_tan.cpp
+++ Unit 2/2.2/Some_Math_Function.txt/sin_cos_tan.cpp
@@ -11,11 +11,11 @@ const double pi = acos(-1.0);//定义pi作为π常量;
 
 int main()
 {
-	double db = sin(pi * 45 / 180);//正弦值;	
+  double db = sin(pi * 45 / 180);//正弦值;	
   double db1 = cos(pi * 45 / 180);//余弦值;	
   double db2 = tan(pi * 45 / 180);//正切值;	
   printf("%f %f %f\n", db,db1,db2);
-	system("pause");
-	return 0;
+  system("pause");
+  return 0;
 }
 
