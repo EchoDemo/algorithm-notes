616b9be66ba5a538d3c30967f6e26c621abae175
EchoDemo <1761910310@qq.com>
2017-03-20 08:02:36 +0800

    Update scanf.cpp

--- Unit 2/2.2/scanf.cpp
+++ Unit 2/2.2/scanf.cpp
@@ -11,6 +11,6 @@ int main()
 	scanf("%d%c%s",&a,&c, str);
 	printf("a=%d,c=%c,str=%s\n",a,c,str);//对%d,%f等其他格式符和字符数组使用%s读入的时候以空格和换行行为读入结束标志,而%c是可以读入空格和换行的
 	system("pause");
-  return 0;
+        return 0;
 }
 
