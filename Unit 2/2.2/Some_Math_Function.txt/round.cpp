d0ad02438f45df253a789d2f339f8535250a3fd3
EchoDemo <1761910310@qq.com>
2017-03-20 09:09:00 +0800

    Create round.cpp

---
+++ Unit 2/2.2/Some_Math_Function.txt/round.cpp
@@ -0,0 +1,19 @@
+//四舍五入
+
+
+
+#include "stdafx.h"
+#include<cstdio>
+#include<cstdlib>
+#include<math.h>
+
+int main()
+{
+	double db1 = asin(1);
+	double db2 = acos(-0.1);
+	double db3 = atan(0);
+	printf("%f,%f,%f\n", db1, db2, db3);
+	system("pause");
+	return 0;
+}
+
