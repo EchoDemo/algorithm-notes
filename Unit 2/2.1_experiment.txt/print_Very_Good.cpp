60d7989254ec285b3f6f33d501939a29b4a238c7
EchoDemo <1761910310@qq.com>
2017-03-20 23:22:03 +0800

    Create print_Very_Good.cpp

---
+++ Unit 2/2.1_experiment.txt/print_Very_Good.cpp
@@ -0,0 +1,20 @@
+/*
+  编写一个C语言程序，输出以下信息：
+
+********************
+
+Very Good!
+
+********************
+
+*/
+
+#include "stdafx.h"
+#include<cstdio>
+#include<cstdlib>
+
+int main() {
+    printf("******************** \nVery Good! \n******************** \n");
+    system("pause");
+    return 0;
+}
