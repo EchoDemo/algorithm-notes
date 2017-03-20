e0cbf1018a182650db1ab8c51a894061b5cc7910
EchoDemo <1761910310@qq.com>
2017-03-20 23:26:38 +0800

    Create sum_scanf.cpp

---
+++ Unit 2/2.1_experiment.txt/sum_scanf.cpp
@@ -0,0 +1,16 @@
+/*
+  从键盘上输入两个整数，并输出这两个数的和，即你的任务是计算a+b。
+*/
+
+
+#include "stdafx.h"
+#include<cstdio>
+#include<cstdlib>
+
+int main(){
+   int a,b;
+   scanf("%d  %d",&a,&b);
+   printf("%d",a+b);
+   system("pause");
+   return 0;
+}
