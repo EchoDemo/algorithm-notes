ad87c854b1c834cf84f01b06f74732763921b39d
EchoDemo <1761910310@qq.com>
2017-03-20 08:51:36 +0800

    Create sqrt().cpp

---
+++ Unit 2/2.2/Some_Math_Function.txt/sqrt().cpp
@@ -0,0 +1,17 @@
+//用于求取double类型变量的算术平方根
+
+
+
+#include "stdafx.h"
+#include<cstdio>
+#include<cstdlib>
+#include<math.h>
+
+int main()
+{
+	double db = sqrt(2.0);
+	printf("%f\n", db);
+	system("pause");
+    return 0;
+}
+
