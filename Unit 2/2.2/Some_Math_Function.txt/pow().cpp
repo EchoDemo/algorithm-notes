7b470139c034651824f30e66bcd7c51be6f70530
EchoDemo <1761910310@qq.com>
2017-03-20 08:49:37 +0800

    Create pow().cpp

---
+++ Unit 2/2.2/Some_Math_Function.txt/pow().cpp
@@ -0,0 +1,17 @@
+//返回r^p,pow(double r,double p)
+
+
+
+#include "stdafx.h"
+#include<cstdio>
+#include<cstdlib>
+#include<math.h>
+
+int main()
+{
+	double db = pow(2.0, 3.0);
+	printf("%f\n", db);
+	system("pause");
+  return 0;
+}
+
