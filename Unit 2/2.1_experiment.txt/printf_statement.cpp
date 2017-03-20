158b5e45014f346f569b94e982b0938d92064834
EchoDemo <1761910310@qq.com>
2017-03-20 23:18:20 +0800

    Create printf_statement.cpp

---
+++ Unit 2/2.1_experiment.txt/printf_statement.cpp
@@ -0,0 +1,14 @@
+/*
+  编写一个C程序，要求在屏幕上输出一下一行信息。
+  This is my first c program!
+*/
+
+#include "stdafx.h"
+#include<cstdio>
+#include<cstdlib>
+
+int main(){
+   printf("This is my first c program!");
+   system("pause");
+   return 0; 
+}
