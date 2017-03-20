5aa87533b6041cffd42460c998fdf05b049da1a6
EchoDemo <1761910310@qq.com>
2017-03-20 23:24:59 +0800

    Create sum.cpp

---
+++ Unit 2/2.1_experiment.txt/sum.cpp
@@ -0,0 +1,15 @@
+/*
+  设置3个变量a, b, sum，其中a, b用来存放两个整数，sum用来存放a, b两个数的和，通过赋值（即采用赋值运算符"="）
+  的方式将a初始化为123，b初始化为456，并把两个变量相加的结果赋值给sum。
+*/
+
+#include "stdafx.h"
+#include<cstdio>
+#include<cstdlib>
+
+int main(){
+   int a=123,b=456,sum=0;
+   printf("sum=%d",a+b);
+   system("pause");
+   return 0;
+}
