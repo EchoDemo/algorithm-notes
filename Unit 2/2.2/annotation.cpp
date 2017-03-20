7ac1fb69f2125fe2a44cdf1b0b8d582b75ac3bb2
EchoDemo <1761910310@qq.com>
2017-03-20 08:25:45 +0800

    Update annotation.cpp

--- Unit 2/2.2/annotation.cpp
+++ Unit 2/2.2/annotation.cpp
@@ -1,1 +1,22 @@
+//单行与多行注释；
+
+#include "stdafx.h"
+#include<cstdio>
+#include<cstdlib>
+
+int main()
+{
+	int a, b,c;
+	scanf("%d%d%d", &a, &b,&c);
+	/*a++;  //自增a;
+	b++;    //自增b;
+	a=a*2;*/
+
+	//c++;
+
+	printf("%d %d %d\n", a, b,c);
+	system("pause");
+    return 0;
+}
+
 
