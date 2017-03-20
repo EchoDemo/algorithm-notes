760f169ba8f591de2cb988dc2b698ce7e5c221d1
EchoDemo <1761910310@qq.com>
2017-03-20 23:55:35 +0800

    Create while.cpp

---
+++ Unit 2/2.4loop_structure.txt/while.cpp
@@ -0,0 +1,18 @@
+//当while的判定为真时,则继续执行,对于if语句中的(!=0)和(==0)的技巧同样适用
+
+
+#include "stdafx.h"
+#include<cstdio>
+#include<cstdlib>
+
+int main()
+{
+	int n = 1, sum = 0;
+	while (n <= 100) {
+		sum = sum + n;
+		n++;
+	}
+	printf("sum=%d\n", sum);
+	system("pause");
+	return 0;
+}
