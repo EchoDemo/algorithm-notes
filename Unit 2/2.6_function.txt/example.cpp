82d5705682205f169f5d3c486152d23a6e8d4dda
EchoDemo <1761910310@qq.com>
2017-03-22 20:48:20 +0800

    Create example.cpp

---
+++ Unit 2/2.6_function.txt/example.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include<cstdio>
+#include<cstdlib>
+
+void print1() {
+	printf("hello,");
+}
+
+void print2() {
+	printf("mengmengda!\n");
+}
+
+int main() {
+	print1();
+	print2();
+	system("pause");
+	return 0;
+}
