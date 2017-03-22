610ca0b5ae5731cd280ab1c928baa5dae1a7d677
EchoDemo <1761910310@qq.com>
2017-03-22 14:15:07 +0800

    Update double_dimensional_array_ADD.cpp

--- Unit 2/2.5array.txt/double_dimensional_array_ADD.cpp
+++ Unit 2/2.5array.txt/double_dimensional_array_ADD.cpp
@@ -1,2 +1,43 @@
 //两个二维数组相加
 
+
+
+
+
+#include "stdafx.h"
+#include<cstdio>
+#include<cstdlib>
+
+
+int main() {
+	int a[3][3], b[3][3];
+	for (int i = 0;i < 3;i++) {
+		for (int j = 0;j < 3;j++) {
+			scanf("%d", &a[i][j]);//输入二维数组a的元素；
+		}
+	}
+
+	for (int i = 0;i < 3;i++) {
+		for (int j = 0;j < 3;j++) {
+			scanf("%d", &b[i][j]);//输入二维数组b的元素；
+		}
+	}
+
+	int c[3][3];
+	for (int i = 0;i < 3;i++) {
+		for (int j = 0;j < 3;j++) {
+			c[i][j] = a[i][j] + b[i][j];//求和；
+		}
+	}
+
+	for (int i = 0;i < 3;i++) {
+		for (int j = 0;j < 3;j++) {
+			printf("%d ", c[i][j]);//输出c;
+		}
+		printf("\n");
+	}
+	system("pause");
+	return 0;
+}
+
+
