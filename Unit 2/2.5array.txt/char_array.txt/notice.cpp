24df0ee8f51ba88dc3a4e4ab326474d1bd0d5909
EchoDemo <1761910310@qq.com>
2017-03-22 16:13:11 +0800

    Update notice.cpp

--- Unit 2/2.5array.txt/char_array.txt/notice.cpp
+++ Unit 2/2.5array.txt/char_array.txt/notice.cpp
@@ -1,19 +1,19 @@
 //如果不是使用scanf函数的%s格式或gets_s函数输入字符串（如：getchar）,一定要在每个字符串的末尾加入“\0”，
-//否则printf和puts输出字符串会因无法识别字符串末尾而输出一大堆乱码。
-
-
-
-
+//否则printf和puts输出字符串会因无法识别字符串末尾而输出一大堆乱码,因为在字符数组当中puts和printf就是通过识别'\0'来作为字符串的结尾来输出的。
 #include "stdafx.h"
 #include<cstdio>
 #include<cstdlib>
 
 
 int main() {
+	//char str[15] = { '0' };//0是指整数0，这种形式的初始化会自动添加'\0';
+	//memset(str, 0, sizeof(str));//而这个0是指ASCII值为零，也就是所有的单元都初始化为'\0',只要后来不把最末的'\0'换掉,就不会乱码;
 	char str[15];
-	for (int i = 0;i < 3;i++) {
+	for (int i = 0;i < 4;i++) {
 		str[i] = getchar();
 	}
+	//str[14] = '\0';//自己在字符数组的末尾添加'\0';
+	getchar();
 	puts(str);
 	system("pause");
 	return 0;
