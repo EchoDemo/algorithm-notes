0d5273b96d0eeabc3354dbdeaf1a36ff64e8de74
EchoDemo <1761910310@qq.com>
2017-03-23 16:03:46 +0800

    Update Chinese_triangle.cpp

--- Unit 2/2.5_experime.txt/Chinese_triangle.cpp
+++ Unit 2/2.5_experime.txt/Chinese_triangle.cpp
@@ -15,5 +15,48 @@
 */
 
 
+#include "stdafx.h"
+#include<cstdio>
+#include<cstdlib>
+
+int main() {
+	int n,a[10][10];
+	scanf("%d", &n);
+
+	/*
+	这是另一种方法；
+	for (int i = 0;i < n;i++) {//在i与j相等的时候和j等于0的时候都为1；
+		a[i][0] = a[i][i] = 1;
+	}
+
+	for (int i = 2;i < n;i++)//从第三行开始，除了边界元素，其中的任何一个元素啊a[i][j]等于其上一行的a[i-1][j-1]与a[i-1][j]的和；
+		for (int j = 1;j < i;j++)
+			a[i][j] = a[i - 1][j - 1] + a[i - 1][j];
+
+	for (int i = 0;i < n;i++) //打印输出，当i=j时就换行；
+		for (int j = 0;j <= i;j++) {
+			printf("%d ", a[i][j]);
+			if (i == j)
+				printf("\n");
+	*/
+
+
+	for (int i = 0;i < n;i++) {
+		for (int j = 0;j <= i;j++) {
+			if (i == j || j == 0)
+				a[i][j] = 1;
+			else
+				a[i][j] = a[i - 1][j - 1] + a[i - 1][j];
+			printf("%d ", a[i][j]);
+			if (i == j)
+				printf("\n");
+		}
+	}
+
+	system("pause");
+	return 0;
+}
+
+
 
 
