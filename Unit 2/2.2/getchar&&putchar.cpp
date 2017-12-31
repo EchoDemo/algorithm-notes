64398fe9c86d612b0418c8aba44f9bbba85728ac
EchoDemo <1761910310@qq.com>
2017-12-31 15:04:47 +0800

    Update getchar&&putchar.cpp

--- Unit 2/2.2/getchar&&putchar.cpp
+++ Unit 2/2.2/getchar&&putchar.cpp
@@ -9,12 +9,12 @@ int main()
 	char c1, c2,c3;
 	c1 = getchar();
 	getchar();//并没有进行存储，
-  c2 = getchar();
+        c2 = getchar();
 	c3 = getchar();
 	putchar(c1);
 	putchar(c2);
 	putchar(c3);
 	system("pause");
-  return 0;
+        return 0;
 }
 
