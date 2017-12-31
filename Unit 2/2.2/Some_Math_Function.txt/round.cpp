7cae3f57cee39f27561e736cc8f9010af0d7fccd
EchoDemo <1761910310@qq.com>
2017-12-31 15:40:44 +0800

    Update round.cpp

--- Unit 2/2.2/Some_Math_Function.txt/round.cpp
+++ Unit 2/2.2/Some_Math_Function.txt/round.cpp
@@ -9,10 +9,12 @@
 
 int main()
 {
-	double db1 = asin(1);
-	double db2 = acos(-0.1);
-	double db3 = atan(0);
-	printf("%f,%f,%f\n", db1, db2, db3);
+	double db1 =round(3.40);
+	double db2 =round(3.45);
+	double db3 =round(3.50);
+	double db4 =round(3.55);
+        double db5 =round(3.60);
+	printf("%d,%d,%d,%d,%d\n", (int)db1, (int)db2, (int)db3,(int)db4,(int)db5);
 	system("pause");
 	return 0;
 }
