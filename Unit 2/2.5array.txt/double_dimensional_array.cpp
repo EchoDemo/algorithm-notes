b023ff03f0e70ea2a7536f55ab06f1561dbb7a1b
EchoDemo <1761910310@qq.com>
2018-01-07 15:05:33 +0800

    Update double_dimensional_array.cpp

--- Unit 2/2.5array.txt/double_dimensional_array.cpp
+++ Unit 2/2.5array.txt/double_dimensional_array.cpp
@@ -10,7 +10,7 @@ int main()
 {
 	int i,j,a[5][6] = { {3,1,3},{8,4},{},{1,2,3,4,5} };
 	for (i = 0;i < 5;i++) {
-		for (int j = 0;j < 6;j++) {
+		for (j = 0;j < 6;j++) {
 			printf("%d ", a[i][j]);
 		}
 		printf("\n");
