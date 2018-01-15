5e11f4bfcfd0ad113824c32761dc3a9a73c433e8
EchoDemo <1761910310@qq.com>
2018-01-15 15:38:30 +0800

    Update 日期类.cpp

--- Unit3/3.4_deal_with_dates.txt/日期类.cpp
+++ Unit3/3.4_deal_with_dates.txt/日期类.cpp
@@ -19,3 +19,42 @@
 
 
 
+
+#include "stdafx.h"
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+
+int main() {
+	int n, months[] = { 0,31,28,31,30,31,30,31,31,30,31,30,31 };
+	cin >> n;
+	while (n--) {
+		int i, j, k;
+		cin >> i >> j >> k;
+		if ((j == 12)&&(k==31)) {//如果为12月份且日期为31号，则年数加1，月份变为1月，日数变为1日。
+			i++;
+			j = 1;
+			k = 1;
+		}
+		else {
+			if (k != months[j])
+				k++;//如果日数不等于当月的最大数，则日数直接加1。
+			else {//如果日数等于当月最大数，则月份加1，日数变为1日。
+				j++;
+				k = 1;
+			}
+		}
+		cout << i<<"-";
+		if (j < 10) cout << 0;
+		cout << j << "-";
+		if (k < 10) cout << 0;
+		cout <<k<< endl;
+	}
+	system("pause");
+	return 0;
+}
+
+
+
+
