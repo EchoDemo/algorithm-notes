14ed42eeeb8d24ed3fe123a36952b8b03271e415
EchoDemo <1761910310@qq.com>
2018-01-16 16:19:54 +0800

    Update 1009说反话 (20).cpp

--- Unit3/3.6_string_deal_with.txt/1009说反话 (20).cpp
+++ Unit3/3.6_string_deal_with.txt/1009说反话 (20).cpp
@@ -11,7 +11,9 @@ Standard
 CHEN, Yue
 给定一句英语，要求你编写程序，将句中所有单词的顺序颠倒输出。
 
-输入格式：测试输入包含一个测试用例，在一行内给出总长度不超过80的字符串。字符串由若干单词和若干空格组成，其中单词是由英文字母（大小写有区分）组成的字符串，单词之间用1个空格分开，输入保证句子末尾没有多余的空格。
+输入格式：测试输入包含一个测试用例，在一行内给出总长度不超过80的字符串。字符串由若干单词和若干空格组成，其中单词是由英文字母（大小写有区分）组成的字符
+
+串，单词之间用1个空格分开，输入保证句子末尾没有多余的空格。
 
 输出格式：每个测试用例的输出占一行，输出倒序后的句子。
 
@@ -21,6 +23,8 @@ Hello World Here I Come
 Come I Here World Hello
 */
 
+
+方法一：
 
 
 #include "stdafx.h"
@@ -52,7 +56,39 @@ int main() {
 		for (j = 1;j < b[k][0];j++) {
 			cout << b[k][j];
 		}
-		cout << " ";
+		if (k != 0)
+			cout << " ";
+	}
+	system("pause");
+	return 0;
+}
+
+
+
+方法二：
+
+#include "stdafx.h"
+#include<iostream>
+#include<stdlib.h>
+#include<string.h>
+using namespace std;
+
+int main() {
+	char s[80];
+	gets_s(s);
+	int len = strlen(s);
+	int p = len;
+	for (int i = len - 1; i >= 0; i--) {
+		if (s[i] == ' ') {
+			for (int j = i + 1; j <= p - 1; j++) {
+				cout << s[j];
+			}
+			cout << " ";
+			p = i;
+		}
+	}
+	for (int i = 0; i <= p - 1; i++) {
+		cout << s[i];
 	}
 	system("pause");
 	return 0;
