70aaff83a357e3384a64748106fb3aa8a97b6677
EchoDemo <1761910310@qq.com>
2018-01-18 15:59:15 +0800

    Update A1025. PAT Ranking (25).cpp

--- unit4/4.1_sort.txt/A1025. PAT Ranking (25).cpp
+++ unit4/4.1_sort.txt/A1025. PAT Ranking (25).cpp
@@ -1,9 +1,17 @@
 /*
-Programming Ability Test (PAT) is organized by the College of Computer Science and Technology of Zhejiang University. Each test is supposed to run simultaneously in several places, and the ranklists will be merged immediately after the test. Now it is your job to write a program to correctly merge all the ranklists and generate the final rank.
+Programming Ability Test (PAT) is organized by the College of Computer Science and Technology of Zhejiang University. Each test is 
+
+supposed to run simultaneously in several places, and the ranklists will be merged immediately after the test. Now it is your job to 
+
+write a program to correctly merge all the ranklists and generate the final rank.
 
 Input Specification:
 
-Each input file contains one test case. For each case, the first line contains a positive number N (<=100), the number of test locations. Then N ranklists follow, each starts with a line containing a positive integer K (<=300), the number of testees, and then K lines containing the registration number (a 13-digit number) and the total score of each testee. All the numbers in a line are separated by a space.
+Each input file contains one test case. For each case, the first line contains a positive number N (<=100), the number of test locations.
+
+Then N ranklists follow, each starts with a line containing a positive integer K (<=300), the number of testees, and then K lines contain
+
+ing the registration number (a 13-digit number) and the total score of each testee. All the numbers in a line are separated by a space.
 
 Output Specification:
 
@@ -11,7 +19,9 @@ For each test case, first print in one line the total number of testees. Then pr
 
 registration_number final_rank location_number local_rank
 
-The locations are numbered from 1 to N. The output must be sorted in nondecreasing order of the final ranks. The testees with the same score must have the same rank, and the output must be sorted in nondecreasing order of their registration numbers.
+The locations are numbered from 1 to N. The output must be sorted in nondecreasing order of the final ranks. The testees with the same 
+
+score must have the same rank, and the output must be sorted in nondecreasing order of their registration numbers.
 
 Sample Input:
 2
